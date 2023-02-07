08fdb7d3acccd81514a360f8b1d49ecae7c42cf8
Neeraj <neeraj79108@gmail.com>
2023-02-07 23:03:34 +0530

    Time: 6 ms (24.02%), Space: 8.3 MB (30.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,16 +5,13 @@ public:
         int j=0;
         int k = nums.size()-1;
         while(j<=k){
-            if(nums[j] == 0){
-                swap(nums[i], nums[j]);
-                j++;
-                i++;
-            }
-            else if(nums[j] == 2){
-                swap(nums[j], nums[k]); 
-                k--;
-            }
-            else if(nums[j]==1)j++;
+            if(nums[j] == 0)
+                swap(nums[i++], nums[j++]);
+            
+            else if(nums[j]==1) j++;
+            
+            else
+                swap(nums[j], nums[k--]); 
             
         }
     }
