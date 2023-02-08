21057e085961056e507d78ecea374c6b5a405948
Neeraj <neeraj79108@gmail.com>
2023-02-08 17:29:08 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (54.15%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,6 +4,7 @@ public:
         int count=0;
         for(int i=0; i<nums.size();i++){
             if(nums[i]>nums[(i+1)%nums.size()]) count++;
+            if (count == 2) return false;
         }
         return count<=1;
     }
