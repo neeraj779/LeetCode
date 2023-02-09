e4370c47e826c7a7d0ec4428d590fb50e8c75266
Neeraj <neeraj79108@gmail.com>
2023-02-09 18:46:08 +0530

    Time: 16 ms (20.29%), Space: 13.4 MB (98.99%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> v;
-        int l= -1;
-        int h =-1;
-        int start=0;
-        int end=nums.size()-1;
-        int mid = start + (end-start)/2;
+        vector<int> v = {-1,-1};
+        int start = 0;
+        int end=nums.size()-1;        
         
+        int l = search(nums, start, end, target, true);
+        int h = search(nums, start, end, target, false);
+        v[0] = l;
+        v[1] = h;
+        return v;
+    }
+    
+private:
+    int search(vector<int>& nums, int start, int end, int target, bool flag){
+        int result = -1;
+        int mid = start + (end-start)/2;
         while(start<=end){
             if(nums[mid]==target) {
-                l = mid;
-                end = mid-1;
+                result = mid;
+                if(flag) end = mid-1;
+                else start = mid+1;
             }
             else if(target>nums[mid]){
                 start = mid+1;
@@ -21,27 +30,6 @@ public:
             }
             mid = start + (end-start)/2;
         }
-        
-        start=0;
-        end=nums.size()-1;
-        mid = start + (end-start)/2;
-        
-        
-         while(start<=end){
-            if(nums[mid]==target) {
-                h = mid;
-                start = mid+1;
-            }
-            else if(target>nums[mid]){
-                start = mid+1;
-            }
-            else{
-                end= mid-1;
-            }
-            mid = start + (end-start)/2;
-        }
-        v.push_back(l);
-        v.push_back(h);
-        return v;
+        return result;
     }
 };
