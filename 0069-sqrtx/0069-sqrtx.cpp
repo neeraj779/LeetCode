1fc66ff023b9df6d60fa866f760f553b1f11eb42
Neeraj <neeraj79108@gmail.com>
2023-02-14 17:32:54 +0530

    Time: 0 ms (100.00%), Space: 6 MB (62.57%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        
         int s = 0;
         int e = x;
         int ans = -1;
@@ -15,7 +16,6 @@ public:
                 ans=mid; 
                 s = mid+1;
             }
-            
             else e = mid-1;
         }
         return ans;
