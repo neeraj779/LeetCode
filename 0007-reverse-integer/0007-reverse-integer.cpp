b291019ed5ac70edab1d0931cfcb5bc68b1b4daf
Neeraj <neeraj79108@gmail.com>
2023-03-12 10:45:53 +0530

    Time: 0 ms (100.00%), Space: 6 MB (12.42%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,12 +3,10 @@ public:
     int reverse(int x) {
         int ans = 0;
         while(x!=0){
-            int last = x%10;
-            x/=10;
             if(ans > INT_MAX/10) return 0;
             if(ans < INT_MIN/10) return 0;
-            ans = ans*10 + last;
-            
+            ans = ans*10 + x%10;
+            x/=10;
         }
         return ans;
     }
