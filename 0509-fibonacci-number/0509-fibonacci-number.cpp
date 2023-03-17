7471ba0e77c67cd5091fcf8d0b03665d2bceeab3
Neeraj <neeraj79108@gmail.com>
2023-03-17 23:11:55 +0530

    Time: 3 ms (70.81%), Space: 6.3 MB (9.82%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,14 +4,16 @@ public:
         // if(n==0) return 0;
         // if(n==1||n==2) return 1;
         // return fib(n-1)+fib(n-2);
-        if(n==0) return 0;
-        if(n==1||n==2) return 1;
-        vector<int> memo(n+1, 0);
-        memo[1] = 1;
-        memo[2] = 1;
-        for(int i=3; i<=n; i++) {
-            memo[i] = memo[i-1] + memo[i-2];
-        }
-        return memo[n];
+        // if(n==0) return 0;
+        // if(n==1||n==2) return 1;
+        // vector<int> memo(n+1, 0);
+        // memo[1] = 1;
+        // memo[2] = 1;
+        // for(int i=3; i<=n; i++) {
+        //     memo[i] = memo[i-1] + memo[i-2];
+        // }
+        // return memo[n];
+        double phi = (1 + sqrt(5)) / 2;
+        return round((pow(phi, n) - pow(1-phi, n)) / sqrt(5));
     }
 };
