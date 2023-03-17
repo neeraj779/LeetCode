d562fdc9575207d7a2f31bdd3a8b403483482e67
Neeraj <neeraj79108@gmail.com>
2023-03-17 23:22:13 +0530

    Time: 3 ms (70.81%), Space: 6.1 MB (9.82%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,11 +4,9 @@ public:
         static vector<int> memo(3, 0);
         if(n==0) return 0;
         if(n==1||n==2) return 1;
-        
         if(memo.size() <= n) { // memo is too small, expand it
             memo.resize(n+1, 0);
         }
-        
         if(memo[n] == 0) { // n-th Fibonacci number not computed yet
             memo[n] = fib(n-1) + fib(n-2); // compute n-th Fibonacci number recursively
         }
