f28cce24e04bad44889d6e4ffec7306887737134
Neeraj <neeraj79108@gmail.com>
2023-04-09 18:29:55 +0530

    Time: 8 ms (76.46%), Space: 7.7 MB (25.54%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -11,38 +11,38 @@ class Solution
 public:
     ListNode *detectCycle(ListNode *head)
     {
-        map<ListNode*, bool> visited;
-        ListNode* temp = head;
-        while(temp != nullptr){
-            if(visited[temp]==1){
-                return temp;
-            }
-            visited[temp] = 1;
-            temp = temp->next;
-        }
+        // map<ListNode*, bool> visited;
+        // ListNode* temp = head;
+        // while(temp != nullptr){
+        //     if(visited[temp]==1){
+        //         return temp;
+        //     }
+        //     visited[temp] = 1;
+        //     temp = temp->next;
+        // }
         
-//         if (head == NULL)
-//             return head;
+        if (head == NULL)
+            return head;
 
-//         ListNode *slow = head, *fast = head;
+        ListNode *slow = head, *fast = head;
 
-//         while (fast != NULL && fast->next != NULL)
-//         {
-//             fast = fast->next;
-//             if (fast != NULL)
-//                 fast = fast->next;
-//             slow = slow->next;
-//             if (slow == fast)
-//             {
-//                 slow = head;
-//                 while (slow != fast)
-//                 {
-//                     slow = slow->next;
-//                     fast = fast->next;
-//                 }
-//                 return slow;
-//             }
-//         }
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next;
+            if (fast != NULL)
+                fast = fast->next;
+            slow = slow->next;
+            if (slow == fast)
+            {
+                slow = head;
+                while (slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
         return nullptr;
     }
 };
