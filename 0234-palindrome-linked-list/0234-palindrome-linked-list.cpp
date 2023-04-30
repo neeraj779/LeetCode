ac1f6347527db09065186107ac0147d763c93402
Neeraj <neeraj79108@gmail.com>
2023-04-30 18:12:56 +0530

    Time: 277 ms (13.78%), Space: 118.1 MB (50.79%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,22 +8,65 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    bool isPalindrome(ListNode* head) {
-        vector <int> v;
-        ListNode* temp = head;
-        while(temp != NULL){
-            v.push_back(temp->val);
-            temp = temp->next;
-        }
-        
-        int end = v.size()-1;
-        for(int i = 0; i < v.size(); i++){
-            if(v[i] != v[end-i]) return 0;
-        }
-        
-        
-        return 1;
+class Solution
+{
+private:
+  ListNode *getMid(ListNode *head)
+  {
+    ListNode *slow = head;
+    ListNode *fast = head->next;
+    while (fast != NULL && fast->next != NULL)
+    {
+      fast = fast->next->next;
+      slow = slow->next;
     }
+    return slow;
+  }
+
+  ListNode *reverse(ListNode *head)
+  {
+    ListNode *prev = NULL;
+    ListNode *curr = head;
+    ListNode *next = NULL;
+
+    while (curr != NULL)
+    {
+      next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  }
+
+public:
+  bool isPalindrome(ListNode *head)
+  {
+    if (head->next == NULL)
+      return 1;
+
+    // step 1: find middle
+    ListNode *mid = getMid(head);
+
+    // Step 2: reverse list after middle
+    ListNode *temp = mid->next;
+    mid->next = reverse(temp);
+
+    // compare both halfs
+    ListNode *head1 = head;
+    ListNode *head2 = mid->next;
+    while(head2 != NULL)
+    {
+      if (head1->val != head2->val){
+        return 0;
+      }
+        head1 = head1->next;
+        head2 = head2->next;
+    
+    }
+      // step 4: revert list back(repeat step 2)
+       temp = mid->next;
+       mid->next = reverse(temp);
+    return 1;
+  }
 };
