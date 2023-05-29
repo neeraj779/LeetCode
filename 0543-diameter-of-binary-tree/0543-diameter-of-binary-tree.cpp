aaa13b6f94ec466fb0a4f7ddd06dbbc5eff47ae7
Neeraj <neeraj79108@gmail.com>
2023-05-29 19:51:56 +0530

    Time: 18 ms (30.52%), Space: 20.8 MB (6.78%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,20 +11,40 @@
  */
 class Solution {
 private:
-    int height(TreeNode* root){
-        if(!root) return 0;
-        int left = height(root->left);
-        int right = height(root->right);
-        return max(left, right) + 1;
+    // int height(TreeNode* root){
+    //     if(!root) return 0;
+    //     int left = height(root->left);
+    //     int right = height(root->right);
+    //     return max(left, right) + 1;
+    // }
+    
+    pair<int, int> fastDiameterOfBinaryTree(TreeNode* root){
+        if(!root){
+            pair<int, int> p = make_pair(0,0);
+            return p;
+        }
+        
+        pair<int, int> left = fastDiameterOfBinaryTree(root->left);
+        pair<int, int> right = fastDiameterOfBinaryTree(root->right);
+        
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second ;
+        
+        pair<int, int> ans;
+        ans.first = max(op1, max(op2, op3));
+        ans.second = max(left.second, right.second)  + 1;
+        return ans;
     }
     
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        if(!root) return 0;
-        int op1 = diameterOfBinaryTree(root->left);
-        int op2 = diameterOfBinaryTree(root->right);
-        int op3 = height(root->left) + height(root->right);
-        int ans = max(op1, max(op2, op3));
-        return ans;
+        // if(!root) return 0;
+        // int op1 = diameterOfBinaryTree(root->left);
+        // int op2 = diameterOfBinaryTree(root->right);
+        // int op3 = height(root->left) + height(root->right);
+        // int ans = max(op1, max(op2, op3));
+        
+        return fastDiameterOfBinaryTree(root).first;
     }
 };
