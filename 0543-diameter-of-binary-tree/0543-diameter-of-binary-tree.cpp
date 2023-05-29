3990391c3c7153d7379665a255445b7d72844fea
Neeraj <neeraj79108@gmail.com>
2023-05-29 19:53:03 +0530

    Time: 15 ms (41.06%), Space: 20.8 MB (6.78%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,7 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 private:
     // int height(TreeNode* root){
     //     if(!root) return 0;
@@ -17,34 +18,37 @@ private:
     //     int right = height(root->right);
     //     return max(left, right) + 1;
     // }
-    
-    pair<int, int> fastDiameterOfBinaryTree(TreeNode* root){
-        if(!root){
-            pair<int, int> p = make_pair(0,0);
+
+    pair<int, int> fastDiameterOfBinaryTree(TreeNode *root)
+    {
+        if (!root)
+        {
+            pair<int, int> p = make_pair(0, 0);
             return p;
         }
-        
+
         pair<int, int> left = fastDiameterOfBinaryTree(root->left);
         pair<int, int> right = fastDiameterOfBinaryTree(root->right);
-        
+
         int op1 = left.first;
         int op2 = right.first;
-        int op3 = left.second + right.second ;
-        
+        int op3 = left.second + right.second;
+
         pair<int, int> ans;
         ans.first = max(op1, max(op2, op3));
-        ans.second = max(left.second, right.second)  + 1;
+        ans.second = max(left.second, right.second) + 1;
         return ans;
     }
-    
+
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
+    int diameterOfBinaryTree(TreeNode *root)
+    {
         // if(!root) return 0;
         // int op1 = diameterOfBinaryTree(root->left);
         // int op2 = diameterOfBinaryTree(root->right);
         // int op3 = height(root->left) + height(root->right);
         // int ans = max(op1, max(op2, op3));
-        
+
         return fastDiameterOfBinaryTree(root).first;
     }
 };
