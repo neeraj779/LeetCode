d947e65a136d99cdb335f7684a0b9aef20dc112a
Neeraj <neeraj79108@gmail.com>
2023-06-01 15:33:06 +0530

    Time: 3 ms (88.24%), Space: 13.6 MB (43.48%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<vector<int>> verticalTraversal(TreeNode *root)
+    {
+        // Map to store nodes at each horizontal distance and level
+        map<int, map<int, multiset<int>>> nodeMap;
+
+        // Queue to perform level order traversal
+        queue<pair<TreeNode *, pair<int, int>>> q;
+
+        vector<vector<int>> result;
+
+        if (!root)
+            return result;
+
+        // Push the root node with horizontal distance and level as 0
+        q.push(make_pair(root, make_pair(0, 0)));
+
+        // Perform level order traversal
+        while (!q.empty())
+        {
+            // Get the front node and its associated horizontal distance and level
+            pair<TreeNode *, pair<int, int>> temp = q.front();
+            q.pop();
+
+            TreeNode *currentNode = temp.first;
+            int horizontalDistance = temp.second.first;
+            int level = temp.second.second;
+
+            // Store the node value in the map
+            nodeMap[horizontalDistance][level].insert(currentNode->val);
+
+            // Enqueue left child if it exists, with updated horizontal distance and level
+            if (currentNode->left)
+                q.push(make_pair(currentNode->left, make_pair(horizontalDistance - 1, level + 1)));
+
+            // Enqueue right child if it exists, with updated horizontal distance and level
+            if (currentNode->right)
+                q.push(make_pair(currentNode->right, make_pair(horizontalDistance + 1, level + 1)));
+        }
+
+        // Traverse the map and build the result vector
+        for (auto i : nodeMap) {
+            vector<int> temp;
+            for (auto j : i.second) {
+                for (auto k : j.second) {
+                    temp.push_back(k);
+                }
+            }
+            result.push_back(temp);
+        }
+        return result;
+    }
+};
