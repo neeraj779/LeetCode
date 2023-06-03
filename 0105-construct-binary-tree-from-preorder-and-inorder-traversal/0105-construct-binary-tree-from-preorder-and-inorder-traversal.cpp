1ce0b2aabca1586caf03daf290694c7718db9250
Neeraj <neeraj79108@gmail.com>
2023-06-03 22:54:17 +0530

    Time: 25 ms (56.45%), Space: 26.4 MB (42.46%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,27 +12,36 @@
 class Solution
 {
 private:
-    int findPosition(const vector<int> &inorder, int target)
+    // int findPosition(const vector<int> &inorder, int target)
+    // {
+    //     for (int i = 0; i < inorder.size(); ++i)
+    //     {
+    //         if (inorder[i] == target)
+    //             return i;
+    //     }
+    //     return -1;
+    // }
+
+    void createMapping(const vector<int> &inorder, map<int, int> &nodeToIndex)
     {
         for (int i = 0; i < inorder.size(); ++i)
         {
-            if (inorder[i] == target)
-                return i;
+            nodeToIndex[inorder[i]] = i;
         }
-        return -1;
     }
 
-    TreeNode *buildTreeHelper(const vector<int> &preorder, const vector<int> &inorder, int &preorderIndex, int preorderEnd, int inorderStart, int inorderEnd)
+    TreeNode *buildTreeHelper(const vector<int> &preorder, const vector<int> &inorder, int &preorderIndex, int preorderEnd, int inorderStart, int inorderEnd, map<int, int> &nodeToIndex)
     {
         if (preorderIndex >= preorderEnd || inorderStart > inorderEnd)
             return nullptr;
 
         int rootValue = preorder[preorderIndex++];
         TreeNode *root = new TreeNode(rootValue);
-        int rootPosition = findPosition(inorder, rootValue);
+        // int rootPosition = findPosition(inorder, rootValue);
+        int rootPosition = nodeToIndex[rootValue];
 
-        root->left = buildTreeHelper(preorder, inorder, preorderIndex, preorderEnd, inorderStart, rootPosition - 1);
-        root->right = buildTreeHelper(preorder, inorder, preorderIndex, preorderEnd, rootPosition + 1, inorderEnd);
+        root->left = buildTreeHelper(preorder, inorder, preorderIndex, preorderEnd, inorderStart, rootPosition - 1, nodeToIndex);
+        root->right = buildTreeHelper(preorder, inorder, preorderIndex, preorderEnd, rootPosition + 1, inorderEnd, nodeToIndex);
 
         return root;
     }
@@ -41,7 +50,9 @@ public:
     TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
     {
         int preorderIndex = 0;
-        TreeNode *root = buildTreeHelper(preorder, inorder, preorderIndex, preorder.size(), 0, inorder.size() - 1);
+        map<int, int> nodeToIndex;
+        createMapping(inorder, nodeToIndex);
+        TreeNode *root = buildTreeHelper(preorder, inorder, preorderIndex, preorder.size(), 0, inorder.size() - 1, nodeToIndex);
         return root;
     }
 };
