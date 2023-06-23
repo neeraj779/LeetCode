e12101dc2cd2741b1d9ecbf9650e81b479ab06cc
Neeraj <neeraj79108@gmail.com>
2023-06-23 22:50:58 +0530

    Time: 28 ms (12.55%), Space: 5.9 MB (85.12%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long int rev = 0;
-        int copy=x;
-        if(x<0)
-            return 0; // negative numbers are not palindrome
-        while(x!=0){
-            rev = rev*10 + x%10;
-            x/=10;
+        if(x<0) return 0;
+        long long int res = 0;
+        int copy = x;
+        while(x){
+            res = res*10 + x%10;
+            x /= 10;
         }
-        return copy==rev;
+        return(res==copy);
     }
 };
