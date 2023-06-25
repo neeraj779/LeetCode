e091ca618e16aaa28dcecde1ed7a0ee68d97235f
Neeraj <neeraj79108@gmail.com>
2023-06-26 00:11:56 +0530

    Time: 29 ms (22.41%), Space: 17.9 MB (90.50%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        
+         // approach 1
 //         int n = nums.size();
 //         for(int i=1; i<=n; ++i){
 //             bool flag = 0;
-            
+
 //             for(int j=0; j<n; j++){
 //                 if(nums[j] == i){
 //                     flag = 1;
@@ -15,16 +17,26 @@ public:
 //         }
 //         return 0;
         
-        int n = nums.size();
-        vector<int> hashTable(n+1, 0);
         
-        for(int i=0; i<n; ++i){
-            hashTable[nums[i]] = 1;
-        }
+        // approach 2
+//         int n = nums.size();
+//         vector<int> hashTable(n+1, 0);
+        
+//         for(int i=0; i<n; ++i){
+//             hashTable[nums[i]] = 1;
+//         }
             
-        for(int i=0; i<=n; ++i){
-            if(hashTable[i] == 0) return i;
-        }
-        return 0;
+//         for(int i=0; i<=n; ++i){
+//             if(hashTable[i] == 0) return i;
+//         }
+//         return 0;
+        
+        
+        // approach 3
+        int n = nums.size();
+        int sum = n*(n+1)/2;
+        int numsSum = accumulate(nums.begin(), nums.end(), 0);
+        return sum - numsSum;
+        
     }
 };
