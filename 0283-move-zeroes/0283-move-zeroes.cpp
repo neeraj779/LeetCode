338927562f28e5895819aa5e90364e7269c368c2
Neeraj <neeraj79108@gmail.com>
2023-06-25 15:16:50 +0530

    Time: 20 ms (91.66%), Space: 19.2 MB (32.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,7 +4,7 @@ public:
         int nonZero = 0;
         for(int j=0; j<nums.size(); j++){
             if(nums[j]!=0){
-                swap(nums[j],nums[nonZero++] );
+                swap(nums[nonZero++], nums[j]);
             }
         }
     }
