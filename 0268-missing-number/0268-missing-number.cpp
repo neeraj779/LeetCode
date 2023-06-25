ccf350143259d193fae11588c51e5282e05fd072
Neeraj <neeraj79108@gmail.com>
2023-06-26 00:21:37 +0530

    Time: 23 ms (51.90%), Space: 18 MB (14.12%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -39,25 +39,28 @@ public:
         // return sum - numsSum;
         
         // approach 4
-        int xor1{0};
-        int xor2{0};
-        int n = nums.size();
-        for(int i = 1; i<=n; ++i)
-            xor1 ^= i;
+//         int xor1{0};
+//         int xor2{0};
+//         int n = nums.size();
+//         for(int i = 1; i<=n; ++i)
+//             xor1 ^= i;
         
-        for(int i = 0; i<n; ++i)
-            xor2 ^= nums[i];
+//         for(int i = 0; i<n; ++i)
+//             xor2 ^= nums[i];
         
-        return xor1 ^ xor2;    
+//         return xor1 ^ xor2;    
         
         
 //             // approach 4.1
-//             int xor1{0};
-//             int xor2{0};
-//             int n = nums.size();
-        
-//         for(int i=0; i<n; ++i){
-//             xor1 ^= i;
-//         }
+            int xor1{0};
+            int xor2{0};
+            int n = nums.size();
+
+            for(int i=0; i<n; ++i){
+                xor1 ^= i+1;
+                xor2 ^= nums[i];
+            }
+
+        return xor1^xor2;
     }
 };
