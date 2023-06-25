2a7bb8dff1d2eb4cb67f46f1c6d155303735222c
Neeraj <neeraj79108@gmail.com>
2023-06-26 00:20:19 +0530

    Time: 25 ms (42.06%), Space: 17.9 MB (90.50%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -46,8 +46,18 @@ public:
             xor1 ^= i;
         
         for(int i = 0; i<n; ++i)
-            xor1 ^= nums[i];
+            xor2 ^= nums[i];
         
         return xor1 ^ xor2;    
+        
+        
+//             // approach 4.1
+//             int xor1{0};
+//             int xor2{0};
+//             int n = nums.size();
+        
+//         for(int i=0; i<n; ++i){
+//             xor1 ^= i;
+//         }
     }
 };
