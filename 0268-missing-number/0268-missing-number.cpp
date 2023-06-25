fdda89b98ba2f110de235dd89708863ecd1575f2
Neeraj <neeraj79108@gmail.com>
2023-06-26 00:22:37 +0530

    Time: 25 ms (42.06%), Space: 17.8 MB (90.50%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,66 +1,65 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        
-         // approach 1
-//         int n = nums.size();
-//         for(int i=1; i<=n; ++i){
-//             bool flag = 0;
+    int missingNumber(vector<int> &nums)
+    {
 
-//             for(int j=0; j<n; j++){
-//                 if(nums[j] == i){
-//                     flag = 1;
-//                     break;
-//                 }
-//             }
-//             if(flag==0) return i;
-//         }
-//         return 0;
-        
-        
+        // approach 1
+        //         int n = nums.size();
+        //         for(int i=1; i<=n; ++i){
+        //             bool flag = 0;
+
+        //             for(int j=0; j<n; j++){
+        //                 if(nums[j] == i){
+        //                     flag = 1;
+        //                     break;
+        //                 }
+        //             }
+        //             if(flag==0) return i;
+        //         }
+        //         return 0;
+
         // approach 2
-//         int n = nums.size();
-//         vector<int> hashTable(n+1, 0);
-        
-//         for(int i=0; i<n; ++i){
-//             hashTable[nums[i]] = 1;
-//         }
-            
-//         for(int i=0; i<=n; ++i){
-//             if(hashTable[i] == 0) return i;
-//         }
-//         return 0;
-        
-        
+        //         int n = nums.size();
+        //         vector<int> hashTable(n+1, 0);
+
+        //         for(int i=0; i<n; ++i){
+        //             hashTable[nums[i]] = 1;
+        //         }
+
+        //         for(int i=0; i<=n; ++i){
+        //             if(hashTable[i] == 0) return i;
+        //         }
+        //         return 0;
+
         // approach 3
         // int n = nums.size();
         // int sum = n*(n+1)/2;
         // int numsSum = accumulate(nums.begin(), nums.end(), 0);
         // return sum - numsSum;
-        
+
         // approach 4
-//         int xor1{0};
-//         int xor2{0};
-//         int n = nums.size();
-//         for(int i = 1; i<=n; ++i)
-//             xor1 ^= i;
-        
-//         for(int i = 0; i<n; ++i)
-//             xor2 ^= nums[i];
-        
-//         return xor1 ^ xor2;    
-        
-        
-//             // approach 4.1
-            int xor1{0};
-            int xor2{0};
-            int n = nums.size();
+        //         int xor1{0};
+        //         int xor2{0};
+        //         int n = nums.size();
+        //         for(int i = 1; i<=n; ++i)
+        //             xor1 ^= i;
 
-            for(int i=0; i<n; ++i){
-                xor1 ^= i+1;
-                xor2 ^= nums[i];
-            }
+        //         for(int i = 0; i<n; ++i)
+        //             xor2 ^= nums[i];
 
-        return xor1^xor2;
+        //         return xor1 ^ xor2;
+
+        //              approach 4.1
+        int xor1{0};
+        int xor2{0};
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i)
+        {
+            xor1 ^= i + 1;
+            xor2 ^= nums[i];
+        }
+
+        return xor1 ^ xor2;
     }
 };
