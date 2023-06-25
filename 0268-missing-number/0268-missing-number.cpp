2bbe11d927a0818e8617968d28a4445c907829e6
Neeraj <neeraj79108@gmail.com>
2023-06-25 23:57:57 +0530

    Time: 116 ms (5.16%), Space: 18.4 MB (9.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        for(int i=1; i<=n; ++i){
-            bool flag = 0;
+//         int n = nums.size();
+//         for(int i=1; i<=n; ++i){
+//             bool flag = 0;
             
-            for(int j=0; j<n; j++){
-                if(nums[j] == i){
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag==0) return i;
+//             for(int j=0; j<n; j++){
+//                 if(nums[j] == i){
+//                     flag = 1;
+//                     break;
+//                 }
+//             }
+//             if(flag==0) return i;
+//         }
+//         return 0;
+        
+        int n = nums.size();
+        vector<int> hashTable(n+1, 0);
+        
+        for(int i=0; i<n; ++i){
+            hashTable[nums[i]] = 1;
+        }
+        
+        for(int i:hashTable) cout<<i<<" ";
+        
+        for(int i=0; i<=n; ++i){
+            if(hashTable[i] == 0) return i;
         }
         return 0;
     }
