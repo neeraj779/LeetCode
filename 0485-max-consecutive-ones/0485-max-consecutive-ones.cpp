4cfd67e857b4329dcd67d26e690849e3bc7b96b3
Neeraj <neeraj79108@gmail.com>
2023-06-26 22:49:08 +0530

    Time: 44 ms (54.74%), Space: 36 MB (94.46%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -5,11 +5,13 @@ public:
         int con{0};
         int maxi{0};
         for(int i=0; i<n; ++i){
-                if(nums[i] == 1)
+                if(nums[i] == 1){
                     con++;
+                    maxi = max(maxi, con);
+                }
                 else
                     con = 0;
-                if(con>maxi) maxi = con;
+                // if(con>maxi) maxi = con;
             }
         return maxi;
     }
