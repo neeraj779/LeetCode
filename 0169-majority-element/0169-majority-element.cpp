8b0cad871447da3625d5eb2eeb43bd7386e8506a
Neeraj <neeraj79108@gmail.com>
2023-06-28 12:48:20 +0530

    Time: 19 ms (79.46%), Space: 19.4 MB (95.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // Boyer-Moore Majority Voting Algorithm
         int n = nums.size();
-        map<int, int> m;
-        for(int i=0; i<n; ++i){
-            m[nums[i]]++;
+        int ele{0}, cnt{0};
+        for(int i = 0; i < n; ++i){
+            if(cnt == 0){
+                ele = nums[i];
+                cnt = 1;
+            }
+            else if(nums[i] == ele)
+                cnt++;
+            else
+                cnt--;
         }
-        
-        for (auto it : m) {
-            if (it.second > (n / 2)) 
-                return it.first;
-        }      
-        
-        return -1;
+        return ele;
     }
 };
