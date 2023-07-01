03579afc1dfcd31d4d408c8120ff22b097c1d3a3
Neeraj <neeraj79108@gmail.com>
2023-07-01 23:26:05 +0530

    Time: 7 ms (83.54%), Space: 14 MB (31.84%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int res{0};
-        int n = operations.size();
-        for(int i=0; i<n; ++i){
-            if(operations[i][1] == '+') // middle char for sign
-                res++;
-            else
-                res--;
-        }
+        // int n = operations.size();
+        // for(int i=0; i<n; ++i){
+        //     if(operations[i][1] == '+') // middle char for sign
+        //         res++;
+        //     else
+        //         res--;
+        // }
+        
+        for(auto i:operations)
+            res += (44 - i[1]);
         return res;
     }
 };
