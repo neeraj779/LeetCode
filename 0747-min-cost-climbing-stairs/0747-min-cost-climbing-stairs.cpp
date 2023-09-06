b8974536ed88aa492294abee83bb8bcabce564d7
Neeraj <neeraj79108@gmail.com>
2023-09-06 11:33:25 +0530

    Time: 0 ms (100%), Space: 13.9 MB (20.01%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -33,6 +33,7 @@ public:
         // vector<int> dp(n+1, -1);
         // int ans = min(solve(cost, n-1, dp), solve(cost, n-2, dp));
         // return ans;
+        
         int n = cost.size();
         return (solve2(cost, n));
     }
