d4a9e61f9c99ec49d74714d534a9a1d65cdff7bc
Neeraj <neeraj79108@gmail.com>
2023-09-18 22:04:35 +0530

    Time: 2 ms (56.16%), Space: 6.3 MB (17.22%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -4,8 +4,9 @@ public:
         int low = 1, high = num, mid;
         while(low<=high){
             mid = low + (high - low)/2;
-            if(1LL*mid*mid == num) return 1;
-            else if(1LL*mid*mid < num) low = mid + 1;
+            long long square = 1LL*mid*mid;
+            if(square == num) return 1;
+            else if(square < num) low = mid + 1;
             else high = mid-1;
         }
         return 0;
