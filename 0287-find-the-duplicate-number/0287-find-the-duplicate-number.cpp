af9620ca04d1ea58f60996f21c710000f6d300ff
Neeraj <neeraj79108@gmail.com>
2023-09-19 18:05:30 +0530

    Time: 67 ms (96.9%), Space: 61.4 MB (30.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -12,17 +12,16 @@ public:
 
         int slow = nums[0];
         int fast = nums[0];
-
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        } while (slow != fast);
+        } while(slow != fast);
 
-        slow = nums[0];
-        while (slow != fast) {
+        slow =  nums[0];
+        while(slow != fast){
             slow = nums[slow];
             fast = nums[fast];
         }
-        return fast; 
+        return slow;
     }
 };
