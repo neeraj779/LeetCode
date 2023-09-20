5fbfc23f5713c5070e474170141cbab4721ee24d
Neeraj <neeraj79108@gmail.com>
2023-09-20 16:47:25 +0530

    Time: 3 ms (71.48%), Space: 9.8 MB (13.63%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,7 +5,7 @@ public:
         int m = matrix[0].size();
 
         for(int i = 0; i<n; ++i){
-            if(matrix[i][0] <= target && matrix[i][m-1] >= target)
+            if(target >= matrix[i][0] && target <= matrix[i][m-1])
                 return binary_search(matrix[i].begin(), matrix[i].end(), target);
         }
         return 0;
