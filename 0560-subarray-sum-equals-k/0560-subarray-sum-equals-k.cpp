2c874d6b79885b6cba097bc473dd8d659465b798
Neeraj <neeraj79108@gmail.com>
2023-09-22 18:41:23 +0530

    Time: 95 ms (29.28%), Space: 44.2 MB (42.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -9,7 +9,11 @@ public:
             sum += nums[i];
             if(sum == k) count++;
             int rem =  sum - k;
-            if(prefixSum.find(rem) != prefixSum.end())  count += prefixSum[rem];
+            if(prefixSum.find(rem) != prefixSum.end())
+            // The next line adds the count of subarrays that end at the current position
+            // and have a sum of 'k - rem', effectively extending them to include the
+            // current element and form valid subarrays with a sum of 'k'.
+                count += prefixSum[rem];
             prefixSum[sum]++;
         }
         return count;
