6ff60876b1c68a94484ef3840b7c80a580ce9688
Neeraj <neeraj79108@gmail.com>
2023-09-26 10:34:30 +0530

    Time: 4 ms (47.89%), Space: 7.4 MB (5.22%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        map<char, int> m;
-        for(char i:t){
-            m[i]++;
-        }
-        for(char i:s){
-            m[i]--;
-        }
-
-        for(auto i:m){
-            if(i.second==1) return i.first;
-        }
-        return '-1';
+        s += t;
+        int c = 0;
+        for(char ch:s)
+            c ^=ch;
+        return c;
     }
 };
