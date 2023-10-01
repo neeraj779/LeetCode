4403e9e22669ee96172861186d94c18ffadc1eea
Neeraj <neeraj79108@gmail.com>
2023-10-01 15:11:25 +0530

    Time: 4 ms (23.93%), Space: 7.4 MB (6.67%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -8,15 +8,29 @@ private:
         }
         return ans;
     }
+
+    vector<int> genrateRow(int n){
+        vector<int> v;
+        long long ans = 1;
+        v.push_back(ans);
+        for(int i = 1; i < n; ++i){
+            ans *= n - i;
+            ans /= i;
+            v.push_back(ans);
+        }
+        return v;
+    }
+
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> v;
         for(int i = 1; i <= numRows; ++i){
-            vector<int> temp;
-            for(int j = 1; j <= i; ++j){
-                temp.push_back(nCr(i-1, j-1));
-            }
-            v.push_back(temp);
+            v.push_back(genrateRow(i));
+            // vector<int> temp;
+            // for(int j = 1; j <= i; ++j){
+            //     temp.push_back(nCr(i-1, j-1));
+            // }
+            // v.push_back(temp);
         }
         return v;
     }
