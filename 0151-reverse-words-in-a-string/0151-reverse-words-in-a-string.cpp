9c0133eb19a02e3fab5ed0204be2d303f39b020d
Neeraj <neeraj79108@gmail.com>
2023-10-01 11:39:13 +0530

    Time: 3 ms (86.65%), Space: 8.6 MB (42.72%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,21 +4,15 @@ public:
         stringstream ss(s);
         string word;
         vector<string> words;
-
-        // Tokenize the input string into words
-        while (ss >> word) {
+        while(ss >> word)
             words.push_back(word);
-        }
-
-        // Reverse the order of words
+        
         string reversed;
-        for (auto it = words.rbegin(); it != words.rend(); ++it) {
-            if (!reversed.empty()) {
-                reversed += " "; // Add a single space between words
-            }
-            reversed += *it;
+        for(int i = words.size() - 1; i>=0; --i){
+            if(reversed.empty() == false)
+                reversed += ' ';
+            reversed += words[i];
         }
-
         return reversed;
     }
 };
