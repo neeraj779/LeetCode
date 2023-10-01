890a5b8a06de565473e55e87c4a98d533bd561a2
Neeraj <neeraj79108@gmail.com>
2023-10-01 11:20:37 +0530

    Time: 22 ms (20.61%), Space: 11.5 MB (39.36%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    string reverseWords(string s) {
+    string reverseWords(string s)
+    {
         stack<char> st;
         string res = "";
-        for(int i = 0; i < s.length(); ++i){
-            if(s[i] != ' '){
-                st.push(s[i]);
-            }
 
-            if(s[i] == ' ' || i == s.length() - 1){
-                while(!st.empty()){
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i] != ' ')
+                st.push(s[i]);
+
+            // if the current character is a space 
+            // or we have reached the end of the string
+            if (s[i] == ' ' || i == s.length() - 1)
+            {
+                while (!st.empty())
+                {
                     res.push_back(st.top());
                     st.pop();
                 }
-                if(s[i] == ' ')
+                if (s[i] == ' ')
+                { // if the current character is a space, append it to the result string
                     res += ' ';
+                }
             }
         }
         return res;
