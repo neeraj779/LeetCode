14a3cdfb41a3974fe1fd8d1b936f496f4ddcdce8
Neeraj <neeraj79108@gmail.com>
2023-10-01 16:49:39 +0530

    Time: 5 ms (19.15%), Space: 6.2 MB (30.03%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while(true){
-            if(num <= 9) break;
+        while(num > 9){
             int res = 0;
             while(num){
                 int d = num % 10;
