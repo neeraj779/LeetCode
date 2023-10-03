63aa67d62561a9dc8cde3487ee17499dc32be23d
Neeraj <neeraj79108@gmail.com>
2023-10-03 22:42:36 +0530

    Time: 0 ms (100%), Space: 7.4 MB (27.67%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -5,12 +5,10 @@ class Solution
         {
             int count = 0;
             map<int, int> m;
-            for (int i : nums)
+            for (int i : nums){
+                count += m[i];
                 m[i]++;
-
-            for (auto i : m)
-                count+= (i.second * (i.second -1))/2;
-
+            }
             return count;
         }
 };
