1008dde359d4bef45851f48ce62d6c28dd155129
Neeraj <neeraj79108@gmail.com>
2024-04-12 18:21:19 +0530

    Time: 7 ms (23.44%), Space: 8.7 MB (72.37%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestValidParentheses(std::string s)
+    {
+        stack<int> st;
+        st.push(-1);
+        int max_length = 0;
+
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i] == '(')
+                st.push(i);
+            else
+            {
+                st.pop();
+                if (st.empty())
+                    st.push(i);
+                else
+                    max_length = std::max(max_length, i - st.top());
+            }
+        }
+
+        return max_length;
+    }
+};
