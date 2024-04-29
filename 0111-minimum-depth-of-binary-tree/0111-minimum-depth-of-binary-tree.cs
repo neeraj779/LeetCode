07f970ee6e568ed5d27534dde58b1345058ca054
Neeraj <neeraj79108@gmail.com>
2024-04-29 16:26:19 +0530

    Time: 337 ms (33.47%), Space: 90.9 MB (52.16%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MinDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        if (root.left == null && root.right == null)
+            return 1;
+
+        if (root.left == null)
+            return 1 + MinDepth(root.right);
+
+        if (root.right == null)
+            return 1 + MinDepth(root.left);
+
+        return 1 + Math.Min(MinDepth(root.left), MinDepth(root.right));
+    }
+}
